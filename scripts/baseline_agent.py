af59c3df1e0693fd00d3bee36b56db4d8867b431
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-05 16:45:04 +0900

    add member "direction"
    This is agent's direction.
    up(^),right(>),down(v),left(<)

--- scripts/baseline_agent.py
+++ scripts/baseline_agent.py
@@ -7,6 +7,7 @@ class BaselineAgent:
         self.x_coordinate = x
         self.y_coordinate = y
         self.total_reword = 0
+        self.direction = 'up' # -up,down,right,left
     def get_action(self,input_vector):
         output_list = self.__neurons.get_output(input_vector)
         return output_list.index(max(output_list))
