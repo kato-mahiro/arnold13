826e494965bb4e72a40821ee8299eb3cc5fa1cc1
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-05 14:15:16 +0900

    mv function(get_input_vector) to field.py from main.py
    still under implimenting

--- scripts/field.py
+++ scripts/field.py
@@ -78,6 +78,24 @@ class Field:
                 field_of_view[y][x] = self.grid[adjusted_y][adjusted_x]
         return field_of_view
 
+    def get_input_vector(field_of_view):
+        for i in range(7):
+            print(' '.join(field_of_view[i]))
+        input_vector=[]
+        field_of_view[0] = field_of_view[0][2:-2]
+        field_of_view[1] = field_of_view[1][1:-1]
+        field_of_view[-2] = field_of_view[-2][1:-1]
+        field_of_view[-1] = field_of_view[-1][2:-2]
+        for i in range(7):
+            input_vector += field_of_view[i]
+        input_vector += [1.0] #(bias neuron)
+        input_vector += [random.choice([0.0,1.0])]
+        for i in range(len(input_vector)):
+            if input_vector[i] == '_':
+                input_vector[i] = 0.0
+            elif input_vector[i] == '#':
+                input_vector[i] = 1.0
+
 if __name__=='__main__':
     field = Field()
     for i in range(PREY_NUM):
