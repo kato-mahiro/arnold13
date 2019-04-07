6ef03edddf40731a823aa55ef0d718abdbc8c028
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-07 11:09:26 +0900

    combine 'give_field_of_view' and 'give_input_vector'

--- scripts/field.py
+++ scripts/field.py
@@ -69,17 +69,16 @@ class Field:
                     self.grid[prey.x+1][prey.y] = '#'
                 return
 
-    def give_field_of_view(self, x_coordinate, y_coordinate):
+    def give_input_vector(self, x_coordinate, y_coordinate):
         field_of_view = [ ['_' for i in range(VIEW_RANGE)] for j in range(VIEW_RANGE) ]
+        input_vector=[]
         for y in range(VIEW_RANGE):
             for x in range(VIEW_RANGE):
                 adjusted_x = self.troidal_process(x_coordinate + x - VIEW_RANGE//2)
                 adjusted_y = self.troidal_process(y_coordinate + y - VIEW_RANGE//2)
                 field_of_view[y][x] = self.grid[adjusted_y][adjusted_x]
-        return field_of_view
-
-    def give_input_vector(self, field_of_view):
-        input_vector=[]
+        for i in range(VIEW_RANGE):
+            print(' '.join(field_of_view[i]))
         "cut edge information"
         field_of_view[0] = field_of_view[0][2:-2]
         field_of_view[1] = field_of_view[1][1:-1]
