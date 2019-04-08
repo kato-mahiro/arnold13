b1b617920201f942fdbf511e688821848b5db199
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-08 14:48:57 +0900

    create trial.py
    trial(weights_ih,weights_ho)
    execute 400 time steps trial.
    return total weights which agent have got.

---
+++ scripts/trial.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+import os
+import random
+from baseline_agent import BaselineAgent
+from field import Field
+
+PREY_NUM = 40
+
+def trial(weights_ih,weights_ho):
+    field = Field()
+    for i in range(PREY_NUM):
+        field.add_prey()
+    agent = BaselineAgent(10,10,weights_ih,weights_ho)
+
+    for i in range(400):
+        input_vector = field.give_input_vector(agent.x_coordinate,agent.y_coordinate)
+        action_no = agent.get_action(input_vector)
+        field.position_update(action_no,agent)
+    return agent.total_reword
+
+if __name__=='__main__':
+    trial()
