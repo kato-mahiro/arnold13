2ca50a15745f42b41f2807e3d03c97bd3bbb9aa7
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-08 18:52:25 +0900

    modify main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,28 +1,19 @@
-import time
-start = time.time()
 import random
-import numpy as np
 from deap import base
 from deap import creator
 from deap import tools
+
 from trial import trial
+from const import *
 
-INPUT_NUMBER = 39
-HIDDEN_LAYER_NUMBER = 16
-OUTPUT_NUMBER = 4
-
-#creatorはbaseのクラスを継承して新たなクラスを作成する
 creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 creator.create("Individual", list, fitness=creator.FitnessMax)
 
-#toolboxは関数を作成する
 toolbox = base.Toolbox()
 #attr_uniform,
 toolbox.register("attr_uniform", random.uniform, -2, 2)
-#initRepeatはToolboxにあらかじめ用意されている関数
-#引数1: データを入れるコンテナの型、引数2: 個々のデータを生成する関数、引数3: コンテナのサイズ
 toolbox.register("individual", tools.initRepeat, creator.Individual , toolbox.attr_uniform, \
-                            INPUT_NUMBER*HIDDEN_LAYER_NUMBER + HIDDEN_LAYER_NUMBER*OUTPUT_NUMBER)
+                                INPUT_NUMBER*HIDDEN_LAYER_NUMBER + HIDDEN_LAYER_NUMBER*OUTPUT_NUMBER)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
 #評価関数の作成
@@ -32,14 +23,14 @@ toolbox.register("evaluate", evalOneMax)
 
 #交差、突然変異、選択用の関数
 toolbox.register("mate", tools.cxTwoPoints)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("mutate", tools.mutFlipBit, indpb=0.001)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 def main():
     random.seed(64)
     # 初期の個体群を生成
-    pop = toolbox.population(n=300)
-    CXPB, MUTPB, NGEN = 0.5, 0.2, 40 # 交差確率、突然変異確率、進化計算のループ回数
+    pop = toolbox.population(n=90)
+    CXPB, MUTPB, NGEN = 0.5, 0.2, 1000 # 交差確率、突然変異確率、進化計算のループ回数
 
     print("Start of evolution")
 
@@ -103,5 +94,3 @@ def main():
 
 if __name__=='__main__':
     main()
-    end = time.time()
-    print('所要時間:' ,end-start)
