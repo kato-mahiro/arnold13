acbec475b0a9d60b68c75d07565b570b998e5456
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-08 18:07:01 +0900

    use const.py and function decode.

--- scripts/trial.py
+++ scripts/trial.py
@@ -1,15 +1,17 @@
-#coding:utf-8
-import os
-import random
+"""
+This script provide one trial.
+a trial return result value(total reword which agent have got)
+"""
 from baseline_agent import BaselineAgent
 from field import Field
+from decoder import decoder
+from const import *
 
-PREY_NUM = 40
-
-def trial(weights_ih=None,weights_ho=None):
+def trial(gene):
     field = Field()
     for i in range(PREY_NUM):
         field.add_prey()
+    weights_ih,weights_ho = decoder(gene)
     agent = BaselineAgent(10,10,weights_ih,weights_ho)
 
     for i in range(400):
