1d5a6c5c0c3019f4891ce4e4abad02fe6193b8d6
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-09 12:00:23 +0900

    Squashed commit of the following:
    create baseline agent and trial.
    
    * note *
    - Need to Change behavior when agent turn right or left.
    - Need to Fix genetic algorithm.

---
+++ scripts/neurons.py
@@ -0,0 +1,29 @@
+#coding:utf-8
+import numpy as np
+import math
+from const import *
+
+class Neurons:
+    def __init__(self,weights_ih=None,weights_ho=None):
+        if weights_ih == None and weights_ho == None:
+            #create randomized np.array. range is (-2.0 ~ +2.0).
+            self.weights_ih = 4.0 * np.random.random(( INPUT_NUMBER, HIDDEN_LAYER_NUMBER)) -2.0
+            self.weights_ho = 4.0 * np.random.random(( HIDDEN_LAYER_NUMBER, OUTPUT_NUMBER)) -2.0
+        else:
+            self.weights_ih = np.array(weights_ih)
+            self.weights_ho = np.array(weights_ho)
+    def set_weights(weights_ih,weights_ho):
+        self.weights_ih = weights_ih
+        self.weights_ho = weights_ho
+    def get_output(self, input_vector):
+        output_vector = np.dot(input_vector, self.weights_ih)
+        output_vector = [math.tanh(i) for i in output_vector.tolist()]
+        output_vector = np.dot(output_vector, self.weights_ho)
+        output_vector = [math.tanh(i) for i in output_vector.tolist()]
+        return output_vector
+
+if __name__=='__main__':
+    n = Neurons()
+    print(n.weights_ih)
+    input_vector=[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+    print(n.get_output(input_vector))
---
+++ scripts/trial.py
@@ -0,0 +1,25 @@
+"""
+This script provide one trial.
+a trial return result value(total reword which agent have got)
+"""
+from baseline_agent import BaselineAgent
+from field import Field
+from decoder import decoder
+from const import *
+
+def trial(gene):
+    field = Field()
+    for i in range(PREY_NUM):
+        field.add_prey()
+    weights_ih,weights_ho = decoder(gene)
+    field.set_agent( BaselineAgent(10,10,weights_ih,weights_ho) )
+
+    for i in range(400):
+        input_vector = field.give_input_vector(field.agent.x,field.agent.y)
+        action_no = field.agent.get_action(input_vector)
+        field.position_update(action_no,field.agent)
+    return field.agent.total_reword
+
+if __name__=='__main__':
+    for i in range(100):
+        print(trial())
