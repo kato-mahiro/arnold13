eb32f7fbfd3c2f5de45f77ea4f7fab90f858f5de
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-11 21:33:44 +0900

    change shapes

--- main_model/main_agent.py
+++ main_model/main_agent.py
@@ -2,17 +2,22 @@
 from neurons import Neurons
 import random
 
-class BaselineAgent:
+class MainAgent:
     def __init__(self,x=10,y=10,weights_ih=None,weights_ho=None):
         self.__neurons = Neurons(weights_ih,weights_ho)
         self.x = x
         self.y = y
         self.total_reword = 0
         self.direction = 'up' # -up,down,right,left
+        keys = [0,1,2,3]
+        actions = ['go','jump','turn_right','turn_left']
+        random.shuffle(actions)
+        self.randomized_table = dict(zip(keys ,actions))
     def get_action(self,input_vector):
         output_list = self.__neurons.get_output(input_vector)
         return output_list.index(max(output_list))
 
-class MainAgent(BaselineAgent):
-    def randomization(self):
-        rand 
+if __name__=='__main__':
+    for i in range(10):
+        main_agent = MainAgent()
+        print(main_agent.randomized_table)
