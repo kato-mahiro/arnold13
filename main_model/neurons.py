8182c592afd9187200c79cc28f513d1f39cc6fef
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-11 21:32:59 +0900

    create weights of additional neurons.

--- main_model/neurons.py
+++ main_model/neurons.py
@@ -4,23 +4,30 @@ import math
 from const import *
 
 class Neurons:
-    def __init__(self,weights_ih=None,weights_ho=None):
+    def __init__(self,weights_ih=None,weights_ho=None,):
         if weights_ih == None and weights_ho == None:
             #create randomized np.array. range is (-2.0 ~ +2.0).
-            self.weights_ih = 4.0 * np.random.random(( INPUT_NUMBER, HIDDEN_LAYER_NUMBER)) -2.0
-            self.weights_ho = 4.0 * np.random.random(( HIDDEN_LAYER_NUMBER, OUTPUT_NUMBER)) -2.0
+            self.weights_ih = 4.0 * np.random.random(( INPUT_NUMBER, HIDDEN_NUMBER)) -2.0
+            self.weights_ho = 4.0 * np.random.random(( HIDDEN_NUMBER, OUTPUT_NUMBER)) -2.0
+            self.weights_im = 4.0 * np.random.random(( INPUT_NUMBER, MODURATORY_NUMBER)) -2.0
+            self.weights_mh = 4.0 * np.random.random(( MODURATORY_NUMBER, HIDDEN_NUMBER)) -2.0
+            self.weights_em = 4.0 * np.random.random(( ECHO_NUMBER, MODURATORY_NUMBER)) -2.0
         else:
             self.weights_ih = np.array(weights_ih)
             self.weights_ho = np.array(weights_ho)
-    def set_weights(weights_ih,weights_ho):
-        self.weights_ih = weights_ih
-        self.weights_ho = weights_ho
+            self.weights_im = np.array(weights_im)
+            self.weights_mh = np.array(weights_mh)
+            self.weights_em = np.array(weights_em)
+
+        self.echo_values = [for i in range(ECHO_NUMBER)]
     def get_output(self, input_vector):
         output_vector = np.dot(input_vector, self.weights_ih)
         output_vector = [math.tanh(i) for i in output_vector.tolist()]
         output_vector = np.dot(output_vector, self.weights_ho)
         output_vector = [math.tanh(i) for i in output_vector.tolist()]
         return output_vector
+    def weights_update(self):
+        
 
 if __name__=='__main__':
     n = Neurons()
