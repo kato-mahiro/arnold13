70cfecbace97a0c860fe5f54c7c9c2d43475721d
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-11 23:18:57 +0900

    create weights_update function.
    need to test.

--- main_model/neurons.py
+++ main_model/neurons.py
@@ -19,15 +19,28 @@ class Neurons:
             self.weights_mh = np.array(weights_mh)
             self.weights_em = np.array(weights_em)
 
-        self.echo_values = [for i in range(ECHO_NUMBER)]
+        self.echo_values = [0 for i in range(ECHO_NUMBER)]
     def get_output(self, input_vector):
         output_vector = np.dot(input_vector, self.weights_ih)
         output_vector = [math.tanh(i) for i in output_vector.tolist()]
+
         output_vector = np.dot(output_vector, self.weights_ho)
         output_vector = [math.tanh(i) for i in output_vector.tolist()]
         return output_vector
     def weights_update(self):
-        
+        output_of_moduratory_neurons = np.dot(input_vector, self.weights_im) + np.dot(echo_values, self.weights_em)
+        output_of_moduratory_neurons = [math.tanh(i) for i in output_of_moduratory_neurons.tolist()]
+
+        output_of_hidden_neurons = np.dot(input_vector, self.weights_ih)
+        output_of_hidden_neurons = [math.tanh(i) for i in output_of_hidden_neurons.tolist()]
+
+        output_of_output_neurons = np.dot(output_of_output_neurons, self.weights_ho)
+        output_of_output_neurons = [math.tanh(i) for i in output_of_output_neurons.tolist()]
+
+        for h in range(HIDDEN_NUMBER):
+            for o in range(OUTPUT_NUMBER):
+                delta_w = 0.01 * output_of_moduratory_neurons[h] * output_of_hidden_neurons[h] * output_of_output_neurons[o]
+                self.weights_ho[h][o] += self.weights_ho[h][o] * delta_w
 
 if __name__=='__main__':
     n = Neurons()
