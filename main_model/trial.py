1946999ea79f739adeaa7f823072579e71c3f477
katomasahiro10 <katomasahiro10@gmail.com>
2019-04-13 16:54:01 +0900

    add learning phase
    need to fix initialize agent

--- main_model/trial.py
+++ main_model/trial.py
@@ -4,10 +4,21 @@ a trial return result value(total reword which agent have got)
 """
 from baseline_agent import BaselineAgent
 from field import Field
+
 from decoder import decoder
 from const import *
 
 def trial(gene):
+    #learning_process(don't use reword)
+    toy_field = ToyField()
+    for i in range(PREY_NUM):
+        toy_field.add_prey()
+    weights_ih,weights_ho = decoder(gene) #need to fix
+    toy_field.set_agent( MainAgent
+    for i in range(800):
+        field.one_step_action()
+
+    #trial_process(use reword)
     field = Field()
     for i in range(PREY_NUM):
         field.add_prey()
